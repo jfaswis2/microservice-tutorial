8fe0c93b3dcaf2099bf86c4612d9e78d65c504c1
Jhon Ferney Aristizabal Sanchez <jfaswis2@gmail.com>
2023-02-22 14:03:30 +0100

    fix config server 2

--- moto-service/moto-service/src/main/java/com/moto/service/entidades/Moto.java
+++ moto-service/moto-service/src/main/java/com/moto/service/entidades/Moto.java
@@ -1,9 +1,8 @@
 package com.moto.service.entidades;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+
+
+import javax.persistence.*;
 
 @Entity
 public class Moto {
