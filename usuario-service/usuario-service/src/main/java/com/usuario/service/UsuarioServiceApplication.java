deb1e2b6635cd12f5e66d467a0725f60deac6bba
Jhon Ferney Aristizabal Sanchez <jfaswis2@gmail.com>
2023-02-22 14:21:47 +0100

    fix config server 2

--- usuario-service/usuario-service/src/main/java/com/usuario/service/UsuarioServiceApplication.java
+++ usuario-service/usuario-service/src/main/java/com/usuario/service/UsuarioServiceApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 
-
+@EnableFeignClients
 @SpringBootApplication
 public class UsuarioServiceApplication {
 
